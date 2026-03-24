593119a7d4f09314fcc3e6bc36e2391c14201b81
Adrian <adrzanbar@gmail.com>
2026-03-24 19:47:24 -0300

    Add public files support

--- src/main/webapp/app/entities/file/file.model.ts
+++ src/main/webapp/app/entities/file/file.model.ts
@@ -2,11 +2,7 @@ import { FileVisibility } from 'app/entities/enumerations/file-visibility.model'
 
 export interface IFile {
   id: string;
-<<<<<<< HEAD
-  visibility?: FileVisibility | null;
-=======
   visibility?: keyof typeof FileVisibility | null;
->>>>>>> b2c9e59 (Add public files support)
   url?: string | null;
   createdBy?: string | null;
   createdDate?: string | null;
